ce1fa9cab046277fc9de4de58c221ab21c98dbfa
mikefab <mikefabrikant@gmail.com>
2017-10-21 17:41:46 -0400

    Debug staging

--- api/controllers/general.js
+++ api/controllers/general.js
@@ -239,7 +239,10 @@ export const getSchools = (request, response) => {
   .then(result => {
     console.log(result.rows.length, '^^^^')
     let csv_like_array = result.rows.map((e, i) => {
-      return i === 0 ? Object.keys(e) : Object.values(e)
+      if (i&100===0) {
+        console.log(i, e)
+      }
+      return i === 0 ? Object.keys(e) : Object.values(e);
     });
     console.log(csv_like_array.length, '&&&&&')
     response.json({
