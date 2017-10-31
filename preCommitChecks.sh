ebd5843670f3c8a890cae860a98b44a3be720815
mikefab <mikefabrikant@gmail.com>
2017-10-31 17:38:18 -0400

    run mocha every commit

--- preCommitChecks.sh
+++ preCommitChecks.sh
@@ -11,14 +11,20 @@ fi
 
 STAGED_FILES=($(git diff --cached --name-only --diff-filter=ACM | grep ".jsx\{0,1\}$"))
 
+testFailed=0
+lintFailed=0
+
+$MOCHA  --compilers js:babel-core/register test --recursive
+if [[ $? != 0 ]] ; then
+  testFailed=1
+fi
+
 echo "ESLint'ing ${#STAGED_FILES[@]} files"
 
 if [[ "$STAGED_FILES" = "" ]]; then
   exit 0
 fi
 
-testFailed=0
-lintFailed=0
 for file in ${STAGED_FILES}; do
   git show :$file | $ESLINT --stdin --stdin-filename "$file"
   if [[ $? != 0 ]] ; then
@@ -31,10 +37,6 @@ done;
 
 if [[ $lintFailed == 0 ]]; then
   printf "\n\033[42mJsLint Successfull\033[0m\n"
-  $MOCHA
-  if [[ $? != 0 ]] ; then
-    testFailed=1
-  fi
 else
   printf "\n\033[41mCOMMIT FAILED:\033[0m Fix eslint errors and try again\n"
   exit 1
