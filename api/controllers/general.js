b971c51777210558f04fc9d8934b0ff34521107b
ayanez17 <fredyyanez39@yahoo.com>
2018-01-10 10:19:39 -0500

    added the controller function getSchool

--- api/controllers/general.js
+++ api/controllers/general.js
@@ -16,7 +16,10 @@ export function getMosquito(request, response) {
   // let [ key, kind, country ] = request._key.split('_')
 
   let key = 'mosquito'
-  let {kind: kind, country: country} = getParams(request)
+  let {
+    kind: kind,
+    country: country
+  } = getParams(request)
 
 
   const source = config[key].source
@@ -25,16 +28,18 @@ export function getMosquito(request, response) {
     .getMosquito(key, kind, country)
     .then(data => {
       response.json({
-      key: key,
-      kind: kind,
-      source: source,
-      source_url: source_url,
-      data: data
+        key: key,
+        kind: kind,
+        source: source,
+        source_url: source_url,
+        data: data
+      })
     })
-  })
     .catch(err => {
       logger.logErrorResponse(request, err)
-      response.json({message: err})
+      response.json({
+        message: err
+      })
     })
 }
 
@@ -56,7 +61,7 @@ export function getPopulation(request, response) {
 
   let {
     source = config.population.default_source,
-    country: country
+      country: country
   } = getParams(request)
 
   const data_source = (source !== undefined) ? source : config.population.source
@@ -71,7 +76,9 @@ export function getPopulation(request, response) {
     })
     .catch(err => {
       logger.logErrorResponse(request, err)
-      response.json({message: err})
+      response.json({
+        message: err
+      })
     })
 }
 
@@ -91,7 +98,11 @@ export function getCases(request, response) {
   // const [ key, kind, weekType, week ] = request._key.split('_')
 
   let key = 'cases'
-  let {kind: kind, weekType: weekType, date: week} = getParams(request)
+  let {
+    kind: kind,
+    weekType: weekType,
+    date: week
+  } = getParams(request)
 
   const source = config.cases[kind].source
   const source_url = config.cases[kind].source_url
@@ -100,16 +111,18 @@ export function getCases(request, response) {
     .get_cases(key, kind, weekType, week)
     .then(cases => {
       return response.json({
-      kind: kind,
-      source: source,
-      source_url: source_url,
-      weekType: weekType,
-      cases: cases
+        kind: kind,
+        source: source,
+        source_url: source_url,
+        weekType: weekType,
+        cases: cases
+      })
     })
-  })
     .catch(error => {
       logger.logErrorResponse(request, error)
-      response.json({message: error})
+      response.json({
+        message: error
+      })
     })
 }
 
@@ -124,14 +137,14 @@ export const getCountriesWithSchools = (request, response) => {
   options.group_by = 'country_code';
   return new Promise((resolve, reject) => {
     general_helper.getCountriesWithSchools(options)
-    .then(results => {
-      results.countries = results.rows.map(r => {
-        return r.country_code;
-      }).sort();
-      // Remove "[{country_code": "GT}...]"
-      delete results.rows;
-      response.json(results);
-    })
+      .then(results => {
+        results.countries = results.rows.map(r => {
+          return r.country_code;
+        }).sort();
+        // Remove "[{country_code": "GT}...]"
+        delete results.rows;
+        response.json(results);
+      })
   })
 }
 
@@ -159,21 +172,23 @@ export function getProperties(request, response) {
   }
 
   return general_helper
-  .getProperties(key)
-  .then(properties => {
-    if (properties.properties) {
-      return response.json({
-        key: properties.key,
-        properties: properties.properties
+    .getProperties(key)
+    .then(properties => {
+      if (properties.properties) {
+        return response.json({
+          key: properties.key,
+          properties: properties.properties
+        })
+      } else {
+        return response.json(properties);
+      }
+    })
+    .catch(err => {
+      logger.logErrorResponse(request, err)
+      response.json({
+        message: error
       })
-    } else {
-      return response.json(properties);
-    }
-  })
-  .catch(err => {
-    logger.logErrorResponse(request, err)
-    response.json({message: error})
-  })
+    })
 }
 
 /**
@@ -186,9 +201,9 @@ export const getToken = (request, response) => {
   response.format({
     'text/html': function() {
       response.send('<html><body><h3>Please click <a href="' +
-      url +
-      '"> HERE </a>' +
-      ' and follow next steps to get access token</h3></body></html>')
+        url +
+        '"> HERE </a>' +
+        ' and follow next steps to get access token</h3></body></html>')
     }
   })
 }
@@ -202,13 +217,15 @@ export const getToken = (request, response) => {
 export const getRefreshToken = (request, response) => {
   const code = request.query.code
   auth.getRefreshToken(code)
-  .then(object => {
-    response.format({
-      'text/html': () => {
-        response.json({refresh_token: object.refresh_token})
-      }
+    .then(object => {
+      response.format({
+        'text/html': () => {
+          response.json({
+            refresh_token: object.refresh_token
+          })
+        }
+      })
     })
-  })
 }
 /**
  * Displays the token or error received from Auth0.
@@ -219,16 +236,18 @@ export const refreshToken = (request, response) => {
   let params = getParams(request)
   const refresh_token = params.refresh_token
   auth.refreshAccessToken(refresh_token)
-  .then(object => {
-    if (object.error) {
-      return response.json(object);
-    }
-    response.format({
-      'text/html': () => {
-        response.json({access_token: object.access_token})
+    .then(object => {
+      if (object.error) {
+        return response.json(object);
       }
+      response.format({
+        'text/html': () => {
+          response.json({
+            access_token: object.access_token
+          })
+        }
+      })
     })
-  })
 }
 /**
  * Displays the token or error received from Auth0.
@@ -238,7 +257,7 @@ export const refreshToken = (request, response) => {
 export const showToken = (request, response) => {
   let authObject = qs.parse(request.body)
   let token = (authObject.error) ? authObject.error_description : 'Token:' +
-  authObject.access_token
+    authObject.access_token
   response.format({
     'text/html': function() {
       response.send('<html><body><h3>' + token + '</h3></body></html>')
@@ -252,25 +271,64 @@ export const showToken = (request, response) => {
  * @param{String} response - response object
  */
 export const getSchools = (request, response) => {
-  const {country_code: country} = getParams(request)
+  const {
+    country_code: country
+  } = getParams(request)
   const options = qs.parse(request.query)
   general_helper
-  .getSchools(country, options)
-  .then(result => {
-    let csv_like_array = result.rows.map((e, i) => {
-      return i === 0 ? Object.keys(e) : Object.values(e);
-    });
-    response.json({
-      count: result.count,
-      result: csv_like_array,
-      // result: geojson.parse(result.rows, {Point: ['lat', 'lon']}),
-      hasNext: result.hasNext
+    .getSchools(country, options)
+    .then(result => {
+      let csv_like_array = [Object.keys(result.rows[0])]
+      result.rows.forEach((e, i) => {
+        csv_like_array.push(Object.values(e))
+      });
+      response.json({
+        count: result.count,
+        result: csv_like_array,
+        // result: geojson.parse(result.rows, {Point: ['lat', 'lon']}),
+        hasNext: result.hasNext
+      })
     })
-  })
-  .catch(err => {
-    logger.logErrorResponse(request, err)
-    response.json({message: err})
-  })
+    .catch(err => {
+      logger.logErrorResponse(request, err)
+      response.json({
+        message: err
+      })
+    })
+}
+
+/**
+ * getSchool - gets a school
+ *  @param request request
+ *  @param response response
+ * @return {type}  description
+ */
+export const getSchool = (request, response) => {
+  const {
+    school_id: school_id
+  } = getParams(request)
+  const options = qs.parse(request.query)
+  general_helper
+    .getSchool(school_id, options)
+    .then(result => {
+      console.log(result);
+      let csv_like_array = [Object.keys(result.rows[0])]
+      result.rows.forEach((e, i) => {
+        csv_like_array.push(Object.values(e))
+      });
+      response.json({
+        count: result.count,
+        result: csv_like_array,
+        // result: geojson.parse(result.rows, {Point: ['lat', 'lon']}),
+        hasNext: result.hasNext
+      })
+    })
+    .catch(err => {
+      logger.logErrorResponse(request, err)
+      response.json({
+        message: err
+      })
+    })
 }
 
 /**
