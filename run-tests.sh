0f6a068d5eda306890b4aa1d1d4937e4c9f6212b
Justin W. Flory <jflory@unicef.org>
2018-02-12 16:25:27 -0500

    Rename test suite (run-tests.sh), add comments
    Name is more ambiguous to including all unit tests in the project. The
    tests in this script also run a basic QA step to test the project on
    TravisCI.
    
    Also adds comments to clarify and sort the tests.

--- run-tests.sh
+++ run-tests.sh
@@ -1,24 +1,40 @@
 #!/bin/bash
+#
+# Script to run unit tests on code. Unit tests are created in Mocha:
+#      https://mochajs.org/
+#
+
+
+##### INSTALLATION ######
+
+# Set env variables for eslint and mocha
 cd "$(git rev-parse --show-toplevel)"
 ESLINT="node_modules/.bin/eslint"
 MOCHA="node_modules/.bin/mocha"
 pwd
 
+# Ensure eslint is installed
 if [[ ! -x "$ESLINT" ]]; then
   printf "\t\033[41mPlease install ESlint\033[0m (npm install eslint)\n"
   exit 1
 fi
 
+# Review staged files
 STAGED_FILES=($(git diff --cached --name-only --diff-filter=ACM | grep ".jsx\{0,1\}$"))
 
+
+##### MOCHA / JS UNIT TESTS #####
+
+# Counters for failed tests and lints
 testFailed=0
 lintFailed=0
 
-
+# Run Mocha tests
 $MOCHA  --compilers js:babel-core/register test --recursive
 if [[ $? != 0 ]] ; then
   testFailed=1
 fi
+
 if [[ $testFailed == 0 ]]; then
   printf "\n\033[42mAll test passed\033[0m\n"
   printf "\n\033[42mCOMMIT SUCCEEDED\033[0m\n"
@@ -27,6 +43,8 @@ else
   printf "\n\033[41mCOMMIT FAILED:\033[0m Fix tests and try again\n"
   exit 1
 fi
+
+# Run linting
 echo "ESLint'ing ${#STAGED_FILES[@]} files"
 
 if [[ "$STAGED_FILES" = "" ]]; then
@@ -39,9 +57,6 @@ for file in ${STAGED_FILES}; do
     lintFailed=1
   fi
 done;
-
-# Re-add files since they may have been fixed
-# git add "${STAGED_FILES[@]}"
 
 if [[ $lintFailed == 0 ]]; then
   printf "\n\033[42mJsLint Successfull\033[0m\n"
