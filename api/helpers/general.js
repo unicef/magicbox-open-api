916c157c722f1bba3ebc12107023ec0a3ed48f92
Thoa Ta <thoat@users.noreply.github.com>
2018-06-28 08:30:25 -0400

    'shapefile' to 'series' in mobility/countries endpnt

--- api/helpers/general.js
+++ api/helpers/general.js
@@ -38,7 +38,7 @@ function createArrayOfCountries(filenames) {
     return line.split('/').slice(0, 3) // discard csv in path
   }).reduce((arr, line) => {
     let obj = {
-      'country': line[2], 'source': line[0], 'shapefile': line[1],
+      'country': line[2], 'source': line[0], 'series': line[1],
       // rebuild path in case apps need it later
       'path': `/mobility/sources/${line[0]}/series/${line[1]}/countries/${line[2]}`,
       'version': '/api/v1'
